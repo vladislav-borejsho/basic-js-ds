67de1343f7fa7feca66675a22bb7392903cddc31
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-10-18 01:14:09 +0300

    feat: queue task half

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.node = [];
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.node;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    return this.node.push(value)
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     return this.node.shift()
   }
 }
 
